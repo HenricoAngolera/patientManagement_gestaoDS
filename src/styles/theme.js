f776c7d0fbcba334f062a62265ac78ef1983e722
henrico <henrico.an@gmail.com>
2023-12-24 16:55:23 -0300

    chore: configuring colors and fonts

---
+++ src/styles/theme.js
@@ -0,0 +1,22 @@
+export default {
+  COLORS: {
+    BACKGROUND: "#F6F6F6",
+    BACKGROUND2: "#FFFFFF",
+
+    LETTER: "#000000",
+    PLACEHOLDER: "#C0C0C0",
+    TABLE_TITLE: "#4B4B4B",
+    TABLE_CONTENT: "#474747",
+
+    BLUE: "#136CDC",
+    
+    PURPLE: "#4F1368",
+    PURPLE_ALERT: "#510972",
+    
+    GRAY: "#656565",
+    GRAY_400: "#565656",
+  },
+  FONTS: {
+    SOURCE_SANS: "'Source Sans 3', sans-serif"
+  }
+}
