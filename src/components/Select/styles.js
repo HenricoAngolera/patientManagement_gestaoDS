b1628a526c4c85b7f540b285741a59033e9aa61b
henrico <henrico.an@gmail.com>
2023-12-29 21:01:03 -0300

    style: fixing design of basic info

--- src/components/Select/styles.js
+++ src/components/Select/styles.js
@@ -1,5 +1,22 @@
 import { styled } from "styled-components"
 
 export const Container = styled.div`
+  label {
+    color: ${({ theme }) => theme.COLORS.GRAY};
+  }
 
+  select {
+    padding: 1rem 1.5rem;
+
+    border: 1px solid ${({ theme }) => theme.COLORS.INPUT_BORDER};
+    background: none;
+    font-size: 1.4rem;
+
+    width: 100%;
+
+  }
+
+  select::placeholder {
+    color: ${({ theme }) => theme.COLORS.PLACEHOLDER};
+  }
 `
